d80a9a505adf8e9c22555333ce5a3437dfaf4459
Joey Sagnes <dragonskymine@gmail.com>
2018-03-05 15:31:28 +0100

    image falcon

--- spaceship.js
+++ spaceship.js
@@ -2,15 +2,15 @@ var spaceshipShader;
 
 function initSpaceshipShader() {
 	spaceshipShader = initShaders("spaceship-vs","spaceship-fs");
-    
+
     // active ce shader
     gl.useProgram(spaceshipShader);
 
     // recupere la localisation de l'attribut dans lequel on souhaite acceder aux positions
     spaceshipShader.vertexPositionAttribute = gl.getAttribLocation(spaceshipShader, "aVertexPosition");
-    gl.enableVertexAttribArray(spaceshipShader.vertexPositionAttribute); // active cet attribut 
+    gl.enableVertexAttribArray(spaceshipShader.vertexPositionAttribute); // active cet attribut
 
-    // pareil pour les coordonnees de texture 
+    // pareil pour les coordonnees de texture
     spaceshipShader.vertexCoordAttribute = gl.getAttribLocation(spaceshipShader, "aVertexCoord");
     gl.enableVertexAttribArray(spaceshipShader.vertexCoordAttribute);
 
@@ -49,7 +49,7 @@ function initSpaceshipTexture() {
         gl.bindTexture(gl.TEXTURE_2D, null);
     }
 
-    spaceshipTexture.image.src = "spaceship.png";
+    spaceshipTexture.image.src = "img/falcon.png";
 }
 
 function Spaceship() {
@@ -74,12 +74,12 @@ function Spaceship() {
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	this.vertexBuffer.itemSize = 3;
 	this.vertexBuffer.numItems = 4;
-		
+
 	// position de texture
 	this.coordBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.coordBuffer);
 	var coords = [
-		 0.0, 0.0, 
+		 0.0, 0.0,
 		 1.0, 0.0,
 		 1.0, 1.0,
 		 0.0, 1.0
@@ -88,14 +88,14 @@ function Spaceship() {
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coords), gl.STATIC_DRAW);
 	this.coordBuffer.itemSize = 2;
 	this.coordBuffer.numItems = 4;
-	
+
 	// creation des faces du cube (les triangles) avec les indices vers les sommets
 	this.triangles = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.triangles);
 	var tri = [0,1,2,0,2,3];
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tri), gl.STATIC_DRAW);
     this.triangles.numItems = 6;
-    
+
     console.log("spaceship initialized");
 }
 
@@ -140,5 +140,3 @@ Spaceship.prototype.draw = function() {
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.triangles);
 	gl.drawElements(gl.TRIANGLES, this.triangles.numItems, gl.UNSIGNED_SHORT, 0);
 }
-
-
